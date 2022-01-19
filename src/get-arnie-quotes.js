8efc052619c2896a8e87c74f23318f4e2b8e286b
Greydne Scott <greydenscott@gmail.com>
2022-01-19 17:16:05 +1000

    Implement getArnieQuotes functionality

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,9 +1,15 @@
 const { httpGet } = require('./mock-http-interface');
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
-};
+const getArnieQuotes = async (urls) => Promise.all(urls.map(getQuote));
+
+const getQuote = async (url) => {
+  const response = await httpGet(url);
+  const { message } = JSON.parse(response.body);
+  if (response.status === 200) {
+    return { 'Arnie Quote': message };
+  } 
+  return { 'FAILURE': message };
+}
 
 module.exports = {
   getArnieQuotes,
